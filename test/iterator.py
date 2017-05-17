a261562c17118c14a575b1403591bbf90be5754e
Tom Tucker <tom@ogc.us>
2017-05-17 16:24:01 -0500

    Add iterator unit test case files.

---
+++ test/iterator.py
@@ -0,0 +1,176 @@
+#!/usr/bin/python
+from __future__ import print_function
+from sosdb import Sos
+from shutil import rmtree
+from os import chdir
+from sostestlib import ROOT_DIR, TestContext
+import struct
+
+def print_obj(obj):
+    print("key : {0}  order : {1}".format(obj['key'], obj['order']))
+
+# Destroy previous test data if present
+try:
+    chdir(ROOT_DIR)
+    rmtree('inf_sup', ignore_errors = True)
+except Exception as e:
+    print(e)
+    pass
+
+# Open the test container
+db = Sos.Container()
+db.create('inf_sup')
+db.open('inf_sup')
+
+# Add a partition
+db.part_create("ROOT")
+part = db.part_by_name("ROOT")
+part.state_set("primary")
+
+# Create the test schema
+template = [
+    { "name" : "key", "type" : "uint64", "index" : {} },
+    { "name" : "order", "type" : "uint64" }
+]
+schema = Sos.Schema()
+schema.from_template("inf_sup", template)
+
+# Add the schema to the container
+schema.add(db)
+
+# Add data to test the inf/sup functions
+data = [
+    [ 1,  1 ],
+    [ 1,  2 ],
+    [ 1,  3 ],
+    [ 2,  4 ],
+    [ 2,  5 ],
+    [ 2,  6 ],
+    [ 4,  7 ],
+    [ 4,  8 ],
+    [ 4,  9 ],
+    [ 5, 10 ],
+    [ 5, 11 ],
+    [ 5, 12 ]
+]
+
+for entry in data:
+    obj = schema.alloc()
+    obj[0] = entry[0]
+    obj[1] = entry[1]
+    obj.index_add()
+
+tests = TestContext(desc="AttrIter Unit Tests", detail=True)
+
+key_attr = schema.attr_by_name('key')
+it = key_attr.attr_iter()
+key = Sos.Key(attr=key_attr)
+
+key.set_value(struct.pack("q", 0))
+res = it.find(key)
+tests.test('if key does not exist, then find(key) returns False', (res == False))
+o = it.item()
+tests.test('if find(key) is False, then it.item() returns None', (o == None))
+k = it.key()
+tests.test('if find(key) is False, then it.key() returns None', (k == None))
+
+key.set_value(struct.pack("q", 0))
+res = it.find_inf(key)
+tests.test('if key < min, then find_inf(key) returns False', (res == False))
+o = it.item()
+tests.test('if find_inf(key) is False, then it.item() returns None', (o == None))
+k = it.key()
+tests.test('if find_inf(key) is False, then it.key() returns None', (k == None))
+
+key.set_value(struct.pack("q", 1))
+it.find_inf(key)
+inf = it.item()
+it.find_sup(key)
+sup = it.item()
+tests.test('if key 1 exists, then order of inf(1) <= sup(1)',
+          inf['order'] <= sup['order'])
+
+key.set_value(struct.pack("q", 3))
+it.find_inf(key)
+inf = it.item()
+it.find_sup(key)
+sup = it.item()
+
+tests.test('infinum and supremum correctly identified: inf(3) == 2 and sup(3) == 4',
+           (inf['key'] == 2 and sup['key'] == 4))
+tests.test('infinum and supremem return first dup',
+           (inf['order'] == 4 and sup['order'] == 7))
+
+key.set_value(struct.pack("q", 0))
+it.find_inf(key)
+inf = it.item()
+it.find_sup(key)
+sup = it.item()
+minkey = int(key_attr.min())
+
+tests.test('infinum where key < min is None', (inf == None))
+tests.test('supremem where key < min == min', (sup['key'] == minkey))
+tests.test('order of supremum is 1 when key < min', (sup['order'] == 1))
+
+key.set_value(struct.pack("q", 6))
+it.find_inf(key)
+inf = it.item()
+it.find_sup(key)
+sup = it.item()
+maxkey = int(key_attr.max())
+
+tests.test('infinum where key > max == max', (inf['key'] == maxkey))
+tests.test('order of infinum is first dup (10) when key > max', (inf['order'] == 10))
+tests.test('supremem where key > max is None', (sup == None))
+
+rc = it.prop_set("inf_last_dup", True)
+rc = it.prop_set("sup_last_dup", True)
+key.set_value(struct.pack("q", 3))
+rc = it.find_inf(key)
+inf = it.item()
+tests.test('find_inf with inf_last_dup==True should return last duplicate',
+           (inf['order'] == 6))
+rc = it.find_sup(key)
+sup = it.item()
+tests.test('find_sup with sup_last_dup==True should return last duplicate',
+           (sup['order'] == 9))
+
+# Iterate unique forward
+rc = it.prop_set("unique", True)
+rc = it.begin()
+last_key = 0
+while rc:
+    o = it.item()
+    tests.test('Iterator returns only the unique keys', (last_key != o['key']))
+    rc = it.next()
+
+# Iterate unique backward
+rc = it.end()
+last_key = 0
+while rc:
+    o = it.item()
+    tests.test('Iterator returns only the unique keys', (last_key != o['key']))
+    rc = it.prev()
+
+# Iterate forward
+rc = it.prop_set("unique", False)
+rc = it.begin()
+exp_order = 1
+while rc:
+    o = it.item()
+    tests.test('Iterator returns next object with order {0}'.format(exp_order),
+               (exp_order == o['order']))
+    exp_order += 1
+    rc = it.next()
+
+# Iterate backward
+rc = it.end()
+exp_order = 12
+while rc:
+    o = it.item()
+    tests.test('Iterator returns prev object with order {0}'.format(exp_order),
+               (exp_order == o['order']))
+    exp_order -= 1
+    rc = it.prev()
+
+tests.summarize()
---
+++ test/sostestlib.py
@@ -0,0 +1,30 @@
+ROOT_DIR = "/btrfs/testroot"
+
+class TestContext(object):
+    def __init__(self, desc=None, detail=False):
+        self.fail_count = 0
+        self.pass_count = 0
+        self.verbose = detail
+        self.desc = desc
+
+    def summarize(self):
+        if self.desc:
+            print("{0}".format(self.desc.center(80, ' ')))
+            sep = '-'.ljust(len(self.desc), '-')
+            print("{0}\n".format(sep.center(80, ' ')))
+        total = self.pass_count + self.fail_count
+        print("TOTAL TESTS : {0:>4}".format(total))
+        print("PASSED      : {0:>4} (%{1:0.2f})".format(self.pass_count, (self.pass_count / total) * 100))
+        print("FAILED      : {0:>4} (%{1:0.2f})".format(self.fail_count, (self.fail_count / total) * 100))
+
+    def test(self, assertion, cond):
+        if cond:
+            if self.verbose:
+                print("PASSED : {0}".format(assertion))
+            self.pass_count += 1
+        else:
+            if self.verbose:
+                print("FAILED : {0}".format(assertion))
+            self.fail_count += 1
+
+
