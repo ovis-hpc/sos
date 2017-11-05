765022108c28f99448339db17539c23dbda0a331
Tom Tucker <tom@ogc.us>
2017-11-05 14:46:41 -0600

    Create a SOS Regression Test Suite
    This is a convenience program to run all defined unit tests. When
    a new test is created, it should be added to this suite.

---
+++ test/sos_regress.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+import unittest
+import logging
+import os
+import sys
+
+logger = logging.getLogger(__name__)
+
+from join_test_u16 import JoinTestU16
+from join_test_u32 import JoinTestU32
+from join_test_u64 import JoinTestU64
+from join_test_u32_str_str import JoinTestU32_Str_Str
+from join_test_u32_str_u32 import JoinTestU32_Str_U32
+from join_test_u32_str_str import JoinTestU32_Str_Str
+from filter_test import FilterTestJoin3xU64
+
+tests = [ JoinTestU16,
+          JoinTestU32,
+          JoinTestU64,
+          JoinTestU32_Str_Str,
+          JoinTestU32_Str_U32,
+          FilterTestJoin3xU64
+          ]
+
+if __name__ == "__main__":
+    chkpath = os.getenv("TEST_DATA_DIR")
+    if chkpath is None:
+        print("Please set the TEST_DATA_DIR environment variable" \
+              " to a valid path where test data can created.")
+        sys.exit(1)
+
+    LOGFMT = '%(asctime)s %(name)s %(levelname)s: %(message)s'
+    logging.basicConfig(format=LOGFMT)
+    logger.setLevel(logging.INFO)
+    _pystart = os.environ.get("PYTHONSTARTUP")
+    if _pystart:
+        execfile(_pystart)
+
+    sos_suite = unittest.TestSuite()
+    for t in tests:
+        sos_suite.addTest(unittest.makeSuite(t))
+
+    runner = unittest.TextTestRunner(verbosity=2)
+    runner.run(sos_suite)
