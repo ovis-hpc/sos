f540553d9cedead0dd21dee73bbb8ccce9b543a3
Tom Tucker <tom@ogc.us>
2018-04-15 20:20:34 -0500

    Add Dprint method to SosUnitTest to support debugging test failures
    Using this function like print will allow test-case output to
    be selectively turned on by defining the environment variable
    SOS_TEST_DEBUG.

--- test/sosunittest.py
+++ test/sosunittest.py
@@ -6,9 +6,17 @@ import logging
 import os
 from sosdb import Sos
 
+_sos_test_debug = False
+
+def Dprint(*args):
+    global _sos_test_debug
+    if _sos_test_debug:
+        print(*args)
+
 class SosTestCase(unittest.TestCase):
     @classmethod
     def setUpDb(cls, db_name):
+        global _sos_test_debug
         cls.db = Sos.Container()
         cls.db_name = db_name
         db_path = os.getenv("TEST_DATA_DIR")
@@ -22,6 +30,8 @@ class SosTestCase(unittest.TestCase):
         cls.db.part_create("ROOT")
         root = cls.db.part_by_name("ROOT")
         root.state_set("PRIMARY")
+        if os.environ.get("SOS_TEST_DEBUG"):
+            _sos_test_debug = True
 
     @classmethod
     def tearDownDb(cls):
