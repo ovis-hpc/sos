ovis-hpc/sos

commit bfc23855d21355e746ea88c0c346689434c77fc8

Fix sos_cmd's JSON query results
sos_cmd prints (null) when a value is blank and [##:##:##:##] when the
value is of SOS_TYPE_BYTE_ARRAY. (null) and [##:##:##:##] are invalid in
the JSON format. The patch prints null and "##:##:##:##" for the two
cases, respectively.

sos/src/sos_cmd.c
@@ -591,6 +591,7 @@ void json_row(FILE *outp, sos_schema_t schema, sos_obj_t obj)
 	size_t col_len;
 	char *col_str;
 	static char str[80];
+	char *s;
 	if (!first_row)
 		fprintf(outp, ",\n");
 	first_row = 0;
@@ -606,12 +607,17 @@ void json_row(FILE *outp, sos_schema_t schema, sos_obj_t obj)
 		} else {
 			col_str = malloc(col_len);
 		}
-		if (sos_attr_is_array(attr) && sos_attr_type(attr) != SOS_TYPE_CHAR_ARRAY) {
-			fprintf(outp, "\"%s\" : [%s]", col->name,
-				sos_obj_attr_to_str(obj, attr, col_str, col_len));
+		s = sos_obj_attr_to_str(obj, attr, col_str, col_len);
+		if (!s) {
+			fprintf(outp, "\"%s\" : null", col->name);
 		} else {
-			fprintf(outp, "\"%s\" : \"%s\"", col->name,
-				sos_obj_attr_to_str(obj, attr, col_str, col_len));
+			if (sos_attr_is_array(attr) &&
+					sos_attr_type(attr) != SOS_TYPE_CHAR_ARRAY &&
+					sos_attr_type(attr) != SOS_TYPE_BYTE_ARRAY) {
+					fprintf(outp, "\"%s\" : [%s]", col->name, s);
+			} else {
+				fprintf(outp, "\"%s\" : %s", col->name, s);
+			}
 		}
 		if (col_str != str)
 			free(col_str);

commit 140137958f86da930f2822927aa1c65d3ba55da5

Fix sos_cmd's JSON results of all SOS value types
Commit bfc23855 introduced a bug that sos_cmd prints SOS_TYPE_BYTE_ARRAY
and SOS_TYPE_CHAR_ARRAY without double-quotes. The patch addresses the
bug and the formats of all SOS value types.

sos/src/sos_cmd.c
@@ -591,7 +591,7 @@ void json_row(FILE *outp, sos_schema_t schema, sos_obj_t obj)
 	size_t col_len;
 	char *col_str;
 	static char str[80];
-	char *s;
+	char *s, *o, *ptr;
 	if (!first_row)
 		fprintf(outp, ",\n");
 	first_row = 0;
@@ -611,12 +611,48 @@ void json_row(FILE *outp, sos_schema_t schema, sos_obj_t obj)
 		if (!s) {
 			fprintf(outp, "\"%s\" : null", col->name);
 		} else {
-			if (sos_attr_is_array(attr) &&
-					sos_attr_type(attr) != SOS_TYPE_CHAR_ARRAY &&
-					sos_attr_type(attr) != SOS_TYPE_BYTE_ARRAY) {
-					fprintf(outp, "\"%s\" : [%s]", col->name, s);
-			} else {
+			switch (sos_attr_type(attr)) {
+			case SOS_TYPE_INT16:
+			case SOS_TYPE_INT32:
+			case SOS_TYPE_INT64:
+			case SOS_TYPE_UINT16:
+			case SOS_TYPE_UINT32:
+			case SOS_TYPE_UINT64:
+			case SOS_TYPE_FLOAT:
+			case SOS_TYPE_DOUBLE:
+			case SOS_TYPE_LONG_DOUBLE:
+			case SOS_TYPE_TIMESTAMP:
 				fprintf(outp, "\"%s\" : %s", col->name, s);
+				break;
+			case SOS_TYPE_OBJ:
+			case SOS_TYPE_STRUCT:
+			case SOS_TYPE_JOIN:
+			case SOS_TYPE_BYTE_ARRAY:
+			case SOS_TYPE_CHAR_ARRAY:
+				fprintf(outp, "\"%s\" : \"%s\"", col->name, s);
+				break;
+			case SOS_TYPE_INT16_ARRAY:
+			case SOS_TYPE_INT32_ARRAY:
+			case SOS_TYPE_INT64_ARRAY:
+			case SOS_TYPE_UINT16_ARRAY:
+			case SOS_TYPE_UINT32_ARRAY:
+			case SOS_TYPE_UINT64_ARRAY:
+			case SOS_TYPE_FLOAT_ARRAY:
+			case SOS_TYPE_DOUBLE_ARRAY:
+			case SOS_TYPE_LONG_DOUBLE_ARRAY:
+				fprintf(outp, "\"%s\" : [%s]", col->name, s);
+				break;
+			case SOS_TYPE_OBJ_ARRAY:
+				fprintf(outp, "\"%s\" : [", col->name);
+				o = strtok_r(s, ',', &ptr);
+				fprintf(outp, "\"%s\"", o);
+				for (; o; o = strtok_r(NULL, ',', &ptr)) {
+					fprintf(outp, ",\"%s\"", o);
+				}
+				fprintf(outp, "]");
+				break;
+			default:
+				break;
 			}
 		}
 		if (col_str != str)